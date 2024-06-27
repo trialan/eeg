6075a21854b20d2d77f258374856edc9c2d94ae2
Thomas Rialan <thomasrialan@gmail.com>
2024-06-27 13:49:46 +0200

    implemented PCA+FgMDM

--- tuto.py
+++ tuto.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -13,11 +15,13 @@ from mne.datasets import eegbci
 from mne.decoding import CSP, UnsupervisedSpatialFilter
 from mne.io import concatenate_raws, read_raw_edf
 
+from tqdm import tqdm
+
 """
 First we define our data-related parameters
     - we set tmin and tmax as in the tutorial linked below
-    - runs are [6,10,14] as in the tutorial, but based on physionet page,
-      it would make sense to include [5,9,13] as well.
+    - runs are [6,10,14] as in the tutorial, [5,9,13] tempting to use but
+      represent action, not imagination
     - use a single subject (subject 1) for pipeline setup
 
 Physionet page: https://physionet.org/content/eegmmidb/1.0.0/
@@ -25,25 +29,27 @@ Tutorial: https://mne.tools/dev/auto_examples/decoding/decoding_csp_eeg.html
 """
 
 tmin, tmax = 1.0, 2.0
-subject = 1
 runs = [6, 10, 14]
 
 
-def get_raw_data():
-    """ Get raw EEGMI data from website or locally if already downloaded """
-    raw_fnames = eegbci.load_data(subject, runs)
-    raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
-    eegbci.standardize(raw)  # set channel names
-    montage = make_standard_montage("standard_1005")
-    raw.set_montage(montage)
-    raw.annotations.rename(dict(T1="hands", T2="feet"))
-    raw.set_eeg_reference(projection=True)
-    return raw
+@lru_cache(maxsize=None)
+def get_data():
+    Xs = []
+    ys = []
+    for subject in range(1, 109+1):
+        X, y = get_formatted_data(subject)
+        Xs.extend(X)
+        ys.extend(y)
+    homogenous_ixs = [i for i in range(len(Xs)) if Xs[i].shape==(64, 161)]
+    Xs_homogenous = [Xs[i] for i in homogenous_ixs]
+    ys_homogenous = [ys[i] for i in homogenous_ixs]
+    print(f"We kept {len(Xs_homogenous)} of {len(Xs)} EEG recordings")
+    return np.array(Xs_homogenous, dtype=np.float64), np.array(ys_homogenous)
 
 
-def get_formatted_data():
+def get_formatted_data(subject):
     """ Band pass filter + pick only EEG channels + format as Epoch objects """
-    raw = get_raw_data()
+    raw = get_raw_data(subject)
     raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
     picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
     epochs = Epochs(
@@ -61,7 +67,19 @@ def get_formatted_data():
     return X, y
 
 
-def assemble_classifer(n_components):
+def get_raw_data(subject):
+    """ Get raw EEGMI data from website or locally if already downloaded """
+    raw_fnames = eegbci.load_data(subject, runs)
+    raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
+    eegbci.standardize(raw)  # set channel names
+    montage = make_standard_montage("standard_1005")
+    raw.set_montage(montage)
+    raw.annotations.rename(dict(T1="hands", T2="feet"))
+    raw.set_eeg_reference(projection=True)
+    return raw
+
+
+def assemble_classifer_PCACSPLDA(n_components):
     lda = LinearDiscriminantAnalysis()
     csp = CSP(n_components=n_components, reg=None, log=True, norm_trace=False)
     pca = UnsupervisedSpatialFilter(PCA(n_components), average=False)
@@ -69,22 +87,77 @@ def assemble_classifer(n_components):
     return clf
 
 
-def print_results(clf, X, y, cv):
+def assemble_classifer_CSPLDA(n_components):
+    lda = LinearDiscriminantAnalysis()
+    csp = CSP(n_components=n_components, reg=None, log=True, norm_trace=False)
+    clf = Pipeline([("CSP", csp), ("LDA", lda)])
+    return clf
+
+
+def assemble_classifer_PCAFgMDM(n_components):
+    pca = UnsupervisedSpatialFilter(PCA(n_components), average=False)
+    FgMDM = pyriemann.classification.FgMDM()
+    clf = Pipeline([("PCA", pca), ("FgMDM", FgMDM)])
+    return clf
+
+
+def results(clf, X, y, cv):
     scores = cross_val_score(clf, X, y, cv=cv, n_jobs=None)
     class_balance = np.mean(y == y[0])
     class_balance = max(class_balance, 1.0 - class_balance)
-    print(f"Accuracy: {np.mean(scores)} / Chance level: {class_balance}")
+    #print(f"Accuracy: {np.mean(scores)} / Chance level: {class_balance}")
+    return np.mean(scores)
 
 
 if __name__ == '__main__':
-    X, y = get_formatted_data()
+    X, y = get_data()
     cv = ShuffleSplit(5, test_size=0.2, random_state=42)
+    component_numbers = list(range(1, 50, 5))
+
+    print("CSP+LDA")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        clf = assemble_classifer_CSPLDA(n_components)
+        score = results(clf, X, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='CSP+LDA')
+
+
+    print("PCA+FgMDM")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        n_epochs, n_channels, n_times = X.shape
+        X_reshaped = X.reshape(n_times * n_epochs, n_channels)
+        pca = PCA(n_components=n_components)
+        pca.fit(X_reshaped)
+
+        X_pca = np.array([pca.transform(epoch.T).T for epoch in X])
+        Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X_pca)
+
+        FgMDM = pyriemann.classification.FgMDM()
+        score = results(FgMDM, Xcov, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='PCA+FgMDM')
 
     #'oas' because: https://github.com/pyRiemann/pyRiemann/issues/65
-    X = pyriemann.estimation.Covariances('oas').fit_transform(X)
-    clf = pyriemann.classification.FgMDM()
+    print("FgMDM")
+    Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X)
+    FgMDM = pyriemann.classification.FgMDM()
+    FgMDM_score = results(FgMDM, Xcov, y, cv)
+    plt.axhline(y=FgMDM_score, linestyle='--', label='FgMDM')
 
-    #clf = assemble_classifer(n_components=5)
-    print_results(clf, X, y, cv)
+    print("PCA+CSP+LDA")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        clf = assemble_classifer_PCACSPLDA(n_components)
+        score = results(clf, X, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='PCA+CSP+LDA')
+
+
+    plt.xlabel("Number of components")
+    plt.ylabel("Accuracy (%)")
+    plt.legend()
+    plt.show()
 
 

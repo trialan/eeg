d6976cf4ef82f3819ef714e2abbd1dcb362d0ff6
Thomas Rialan <thomasrialan@gmail.com>
2024-06-27 14:33:01 +0200

    reproduced plot from Xu et. al.

---
+++ plot_reproduction.py
@@ -0,0 +1,53 @@
+from eeg.main import *
+
+
+if __name__ == '__main__':
+    X, y = get_data()
+    cv = ShuffleSplit(5, test_size=0.2, random_state=42)
+    component_numbers = list(range(1, 50, 5))
+
+    print("CSP+LDA")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        clf = assemble_classifer_CSPLDA(n_components)
+        score = results(clf, X, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='CSP+LDA')
+
+
+    print("PCA+FgMDM")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        n_epochs, n_channels, n_times = X.shape
+        X_reshaped = X.reshape(n_times * n_epochs, n_channels)
+        pca = PCA(n_components=n_components)
+        pca.fit(X_reshaped)
+
+        X_pca = np.array([pca.transform(epoch.T).T for epoch in X])
+        Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X_pca)
+
+        FgMDM = pyriemann.classification.FgMDM()
+        score = results(FgMDM, Xcov, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='PCA+FgMDM')
+
+    #'oas' because: https://github.com/pyRiemann/pyRiemann/issues/65
+    print("FgMDM")
+    Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X)
+    FgMDM = pyriemann.classification.FgMDM()
+    FgMDM_score = results(FgMDM, Xcov, y, cv)
+    plt.axhline(y=FgMDM_score, linestyle='--', label='FgMDM')
+
+    print("PCA+CSP+LDA")
+    scores = []
+    for n_components in tqdm(component_numbers):
+        clf = assemble_classifer_PCACSPLDA(n_components)
+        score = results(clf, X, y, cv)
+        scores.append(score)
+    plt.plot(component_numbers, scores, marker='o', linestyle='-', label='PCA+CSP+LDA')
+
+
+    plt.xlabel("Number of components")
+    plt.ylabel("Accuracy (%)")
+    plt.legend()
+    plt.show()

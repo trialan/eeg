0483a47ac81246a268b518fb8976579a11d37387
Thomas Rialan <thomasrialan@gmail.com>
2024-06-28 14:53:23 +0200

    mesh with no bottom surface

--- laplacian.py
+++ laplacian.py
@@ -1,6 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import spharapy.trimesh as tm
-from scipy.spatial import Delaunay
+from scipy.spatial import Delaunay, ConvexHull
 import spharapy.trimesh as trimesh
 import spharapy.spharabasis as sb
 import spharapy.datasets as sd
@@ -42,13 +42,43 @@ def create_triangular_dmesh(xyz_coords):
     """  Create a mesh using the Delaunay triangulation """
     mesh = Delaunay(xyz_coords)
     mesh_eeg = trimesh.TriMesh(mesh.convex_hull, mesh.points)
+    mesh_eeg = remove_bottom_of_the_mesh(mesh_eeg)
     return mesh_eeg
+
+
+def distance(p1, p2):
+    dx =(p1[0] - p2[0])
+    dy =(p1[1] - p2[1])
+    dz =(p1[2] - p2[2])
+    distance = np.sqrt(dx**2 + dy**2 + dz**2)
+    return distance
+
+
+def length_of_longest_edge(triangle, vertlist):
+    p1 = vertlist[triangle[0]]
+    p2 = vertlist[triangle[1]]
+    p3 = vertlist[triangle[2]]
+    el1 = distance(p1, p2)
+    el2 = distance(p1, p3)
+    el3 = distance(p2, p3)
+    return max(el1, el2, el3)
+
+
+def remove_bottom_of_the_mesh(mesh, N=6):
+    """ Visual inspection reveals that the bottom triangles we want to be rid
+        of have the longest edgs. So we remove the N triangles with longest edge """
+    le =  [length_of_longest_edge(t, mesh._vertlist) for t in mesh._trilist]
+    ixs = np.argsort(le)[::-1][:N]
+    ntrilist = [mesh._trilist[ix] for ix in range(len(mesh._trilist)) if ix not in ixs]
+    mesh._trilist = np.array(ntrilist)
+    return mesh
 
 
 def plot_mesh(mesh):
     """ Expects a sphara TriMesh """
-    vertlist = mesh._vertlist
-    trilist = mesh._trilist
+    vertlist = np.array(mesh._vertlist)
+    trilist = np.array(mesh._trilist)
+
     fig = plt.figure()
     fig.subplots_adjust(left=0.02, right=0.98, top=0.98, bottom=0.02)
     ax = fig.add_subplot(projection='3d')
@@ -60,42 +90,45 @@ def plot_mesh(mesh):
     ax.set_aspect('auto')
     ax.plot_trisurf(vertlist[:, 0], vertlist[:, 1], vertlist[:, 2],
                     triangles=trilist, color='lightblue', edgecolor='black',
-                    linewidth=0.5, shade=True)
+                    linewidth=0.5, shade=True, alpha=1)
     plt.show()
 
 
-def plot_basis_functions(dmesh, coords):
-    """ Plot 9 first ones """
-    basisfuncs = compute_scalp_eigenfunctions(coords)
-    figsb1, axes1 = plt.subplots(nrows=3, ncols=3, figsize=(8, 12),
-                                 subplot_kw={'projection': '3d'})
+def plot_basis_functions(mesh_eeg):
+    vertlist = np.array(mesh_eeg._vertlist)
+    trilist = np.array(mesh_eeg._trilist)
 
+    sphara_basis_unit = sb.SpharaBasis(mesh_eeg, 'unit')
+    basis_functions_unit, natural_frequencies_unit = sphara_basis_unit.basis()
+
+    figsb1, axes1 = plt.subplots(nrows=7, ncols=7, figsize=(8, 12),
+                                 subplot_kw={'projection': '3d'})
     for i in range(np.size(axes1)):
-        #colors = np.mean(basisfuncs[dmesh.convex_hull, i + 0], axis=1)
+        colors = np.mean(basis_functions_unit[trilist, i + 0], axis=1)
         ax = axes1.flat[i]
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
         ax.view_init(elev=60., azim=80.)
         ax.set_aspect('auto')
-        trisurfplot = ax.plot_trisurf(dmesh.points[:, 0], dmesh.points[:, 1],
-            dmesh.points[:, 2], triangles=dmesh.convex_hull,
-            cmap=plt.cm.bwr,
-            edgecolor='white', linewidth=0.)
-        #trisurfplot.set_array(colors)
+        trisurfplot = ax.plot_trisurf(vertlist[:, 0], vertlist[:, 1],
+                                      vertlist[:, 2], triangles=trilist,
+                                      cmap=plt.cm.bwr,
+                                      edgecolor='white', linewidth=0.)
+        trisurfplot.set_array(colors)
         trisurfplot.set_clim(-0.15, 0.15)
 
-    #cbar = figsb1.colorbar(trisurfplot, ax=axes1.ravel().tolist(), shrink=0.85,
-        #orientation='horizontal', fraction=0.05, pad=0.05,
-        #anchor=(0.5, -4.5))
+    cbar = figsb1.colorbar(trisurfplot, ax=axes1.ravel().tolist(), shrink=0.85,
+                           orientation='horizontal', fraction=0.05, pad=0.05,
+                           anchor=(0.5, -4.5))
 
     plt.subplots_adjust(left=0.0, right=1.0, bottom=0.08, top=1.0)
     plt.show()
 
 
 if __name__ == '__main__':
-    coords = get_electrode_coordinates()
-    dmesh = create_triangular_dmesh(coords)
-    plot_mesh(dmesh)
-    #plot_basis_functions(dmesh, coords)
+    xyz_coords = get_electrode_coordinates()
+    mesh_eeg = create_triangular_dmesh(xyz_coords)
+    plot_mesh(mesh_eeg)
+    plot_basis_functions(mesh_eeg)
 

e5e0a79ad3bcaeb2d3865c2f2448abf5375f28a5
Thomas Rialan <thomasrialan@gmail.com>
2024-07-03 18:11:06 +0200

    individual norm

--- experiment_writeups/subtract_reststate.py
+++ experiment_writeups/subtract_reststate.py
@@ -20,7 +20,7 @@ from eeg.laplacian import (get_electrode_coordinates,
                            compute_scalp_eigenvectors_and_values,
                            create_triangular_dmesh, ED)
 from eeg.ml import results
-from eeg.data import get_data
+from eeg.data import get_formatted_data, get_data
 
 """
 Goal of this experiment: download the rest-state data and re-run
@@ -42,25 +42,43 @@ gives a score: 0.619916142557652 (i.e. same as without ratio).
 
 This could be because the mean signal is averaged over all subjects, but
 perhaps their individual means aren't so similar.
+
+This current version of the code subtracts the individual's baseline
+state of activity.
+
 """
+
 
 def get_reststate_data(n_subjects=109, bandpass=True):
     return get_data(n_subjects, reststate=True)
 
 
+def get_normalised_data(X_rs, n_subjects=109, bandpass=True):
+    runs = [6, 10, 14]
+    Xs = []
+    ys = []
+    for subject in range(1, n_subjects+1):
+        X, y = get_formatted_data(subject, bandpass, runs)
+        if X.shape == (64, 161):
+            X = X / X_rs[subject - 1]
+        Xs.extend(X)
+        ys.extend(y)
+    homogenous_ixs = [i for i in range(len(Xs)) if Xs[i].shape==(64, 161)]
+    Xs_homogenous = [Xs[i] for i in homogenous_ixs]
+    ys_homogenous = [ys[i] for i in homogenous_ixs]
+    print(f"We kept {len(Xs_homogenous)} of {len(Xs)} EEG recordings")
+    return np.array(Xs_homogenous, dtype=np.float64), np.array(ys_homogenous)
+
 if __name__ == '__main__':
     X_rs, y_rs = get_reststate_data()
-    X, y = get_data()
+    X, y = get_normalised_data(X_rs)
     cv = ShuffleSplit(5, test_size=0.2, random_state=42)
-
-    mean_X_rs = np.mean(X_rs, axis=0)
-    X_corrected = X / mean_X_rs
 
     print("#############  FgMDM  #############")
     Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X)
     FgMDM = pyriemann.classification.FgMDM()
     FgMDM_score = results(FgMDM, Xcov, y, cv)
-    plt.axhline(y=FgMDM_score, linestyle='--', label='FgMDM (tmin/max=-1-4)')
+    plt.axhline(y=FgMDM_score, linestyle='--', label='FgMDM (norm. subj.)')
     plt.legend()
     plt.show()
 

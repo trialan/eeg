dbed051095f1f303a799f6ec128bc3528cbc85ae
Emeka Igwegbu <emeka.igwegbu@gmail.com>
2024-07-17 11:27:29 -0700

    Changes to imports and various tweaks made while testing

--- experiments/channel_selection/channels.py
+++ experiments/channel_selection/channels.py
@@ -17,15 +17,14 @@ from torch.utils.data import DataLoader, TensorDataset, random_split
 import torch.nn.functional as F
 
 from eeg.data import get_data
-from eeg.utils import (results, get_cv,
-                       avg_power_vector,
-                       get_covariances,
-                       set_seed)
+from eeg.utils import results, get_cv, avg_power_vector, get_covariances, set_seed
 from eeg.experiments.eigen_fgmdm import EDFgMDM, OldED
 from eeg.laplacian import compute_scalp_eigenvectors_and_values
-from eeg.ml import (assemble_classifer_PCACSPLDA,
-                    assemble_classifer_CSPLDA,
-                    PCA)
+from eeg.plot_reproduction import (
+    assemble_classifer_PCACSPLDA,
+    assemble_classifer_CSPLDA,
+    PCA,
+)
 
 
 """
@@ -37,8 +36,9 @@ be interesting to try to combine this with Laplacian and get a higher score.
 
 n_channels = 64
 
+
 def get_sorted_channels(X, y, cv):
-    #A bit sketch to do this on train+test, but we'll fix that later
+    # A bit sketch to do this on train+test, but we'll fix that later
     channels = np.array(range(0, n_channels))
     channel_scores = []
     for channel in channels:
@@ -56,14 +56,13 @@ def get_sorted_channels(X, y, cv):
     return sorted_channels
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     X, y = get_data()
     cv = get_cv()
     eigenvectors, eigenvals = compute_scalp_eigenvectors_and_values()
     sorted_channels = get_sorted_channels(X, y, cv)
 
-    1/0
+    1 / 0
 
     """
     scores = []
@@ -89,25 +88,33 @@ if __name__ == '__main__':
     plt.show()
     """
 
+
 def transform_to_eigenbasis(matrix, eigenvectors):
-    """ eigenvectors are rows """
+    """eigenvectors are rows"""
     assert matrix.shape[0] == eigenvectors.shape[0]
     assert eigenvectors.shape[0] == eigenvectors.shape[1]
-    transformed_matrix = np.dot(eigenvectors, matrix) #? not .T
+    transformed_matrix = np.dot(eigenvectors, matrix)  # ? not .T
     return transformed_matrix
 
     scores = []
     for n_components in tqdm(list(range(10, 35))):
         channel_X = X[:, sorted_channels[:n_components], :]
         n_epochs, n_channels, n_times = channel_X.shape
-        X_ed = np.array([transform_to_eigenbasis(epoch, eigenvectors[:n_components, :n_components]) for epoch in channel_X])
-        Xcov = pyriemann.estimation.Covariances('oas').fit_transform(X_ed)
+        X_ed = np.array(
+            [
+                transform_to_eigenbasis(
+                    epoch, eigenvectors[:n_components, :n_components]
+                )
+                for epoch in channel_X
+            ]
+        )
+        Xcov = pyriemann.estimation.Covariances("oas").fit_transform(X_ed)
 
         FgMDM = pyriemann.classification.FgMDM()
         score = results(FgMDM, Xcov, y, cv)
         scores.append(score)
-    plt.plot(list(range(10,35)), scores, marker='o', linestyle='-', label='Laplacian+FgMDM')
+    plt.plot(
+        list(range(10, 35)), scores, marker="o", linestyle="-", label="Laplacian+FgMDM"
+    )
     plt.legend()
     plt.show()
-
-

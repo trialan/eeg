1e7c05836a4a52b2f4dc63d383bb68b88b6460dd
Thomas Rialan <thomasrialan@gmail.com>
2024-08-01 12:17:19 +0200

    debugging tweaks

--- inverseproblem/simultaneous_eeg_fmri/eval.py
+++ inverseproblem/simultaneous_eeg_fmri/eval.py
@@ -8,7 +8,8 @@ from eeg.inverseproblem.simultaneous_eeg_fmri.cnn import FMRI_CNN, train
 from eeg.inverseproblem.simultaneous_eeg_fmri.eeg_data import get_eeg_data
 
 
-root_dir = "/Users/thomasrialan/Documents/code/DS116/"
+#root_dir = "/Users/thomasrialan/Documents/code/DS116/"
+root_dir = "/root/DS116/"
 
 
 def balance_and_shuffle(X, y):

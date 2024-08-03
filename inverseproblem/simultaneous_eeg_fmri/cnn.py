5d931752bbfe058600643948b50b19213559a1ba
Thomas Rialan <thomasrialan@gmail.com>
2024-08-03 20:45:29 +0200

    debugging tweaks

--- inverseproblem/simultaneous_eeg_fmri/cnn.py
+++ inverseproblem/simultaneous_eeg_fmri/cnn.py
@@ -46,16 +46,17 @@ class BV_FMRI_CNN(nn.Module):
         self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv3d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv3d(64, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 7 * 7 * 7, 64)
         self.fc2 = nn.Linear(64, 1)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
+        self.fc1_input_size = 64 * 7 * 7 * 7
+        self.fc1 = nn.Linear(self.fc1_input_size, 64)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = self.pool(self.relu(self.conv3(x)))
-        x = x.view(-1, 64 * 8 * 8 * 4)
+        x = x.view(-1, self.fc1_input_size)
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)

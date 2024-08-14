ecce25e66b191efbc281372050c1165e6d8ca0fe
Thomas Rialan <thomasrialan@gmail.com>
2024-08-14 09:59:33 +0200

    Shin2017A

--- benchmarking.py
+++ benchmarking.py
@@ -1,7 +1,7 @@
 import moabb
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from moabb.datasets import PhysionetMI
+from moabb.datasets import PhysionetMI, Shin2017A
 from moabb.evaluations import WithinSessionEvaluation
 from moabb.paradigms import LeftRightImagery, MotorImagery
 from pyriemann.spatialfilters import CSP
@@ -14,7 +14,6 @@ from eeg.utils import get_cv, avg_power_vector
 
 moabb.set_log_level("error")
 pipelines = {}
-n_channels = 64
 
 from eeg.experiments.eigen_fgmdm import OldED
 from eeg.laplacian import compute_scalp_eigenvectors_and_values
@@ -35,7 +34,7 @@ class EigenDecomp(BaseEstimator, TransformerMixin):
 
 
 if __name__ == '__main__':
-    dataset = PhysionetMI()
+    dataset = Shin2017A()
     paradigm = LeftRightImagery()
     evecs, _ = compute_scalp_eigenvectors_and_values()
     datasets = [dataset]
@@ -46,7 +45,8 @@ if __name__ == '__main__':
 
     scores = []
     std_errs = []
-    for n_evecs in range(3,64):
+    x_vals = list(range(3,30))
+    for n_evecs in x_vals:
         pipelines["Eigen-FgMDM"] = make_pipeline(EigenDecomp(evecs, n_evecs), Covariances("oas"), FgMDM())
 
         results = evaluation.process(pipelines)
@@ -60,15 +60,18 @@ if __name__ == '__main__':
         print(std_err.iloc[0])
         print("##########")
 
-    x = list(range(3, 64))
-    plt.plot(x, scores)
+    plt.plot(x_vals, scores)
     plt.savefig("eigen_fgmdm_range.png")
     print(max(scores))
     print(np.argmax(scores))
     print(x[np.argmax(scores)])
 
     """
+    PhysionetMI
     Result: 0.720683 (0.017255) -- this is SoTA on Aug. 12th, 2024.
     Benchmark: 0.6887513 (0.0179015) -- this is FgMDM
+
+    Shin2017A
     """
+
 
